ab7e387124ed78f3d404f621e4c33d79a0f66760
logicmunna <logic.munna@gmail.com>
2026-01-24 22:37:19 +0600

    New Commit

--- scripts/2. check wrong password demoqa.py
+++ scripts/2. check wrong password demoqa.py
@@ -8,10 +8,14 @@ import time
 
 options = Options()
 options.add_argument('--log-level=3')
+
+
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
 ser = Service(r"C:\chromedriver\chromedriver.exe")
 driver = webdriver.Chrome(service=ser, options=options)
+
+
 
 driver.maximize_window()
 wait = WebDriverWait(driver, 10)
